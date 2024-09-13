4a47e28d85565014609daa310eac898b668f6b64
Bartek <bartosz.papinski00@gmail.com>
2024-09-13 21:39:43 +0200

    setup

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChwesiukBotV2
+{
+    internal class Program
+    {
+        private static DiscordClient Client { get; set; }
+        private static CommandsNextExtension Commands { get; set; }
+
+        static async Task Main(string[] args)
+        {
+            var jsonReader = new JSONReader();
+            await jsonReader.ReadJSON();
+            Console.WriteLine(jsonReader.token);
+
+            var discordConfig = new DiscordConfiguration()
+            {
+                //Intents = DiscordIntents.All,
+                Token = jsonReader.token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+            };
+
+            Client = new DiscordClient(discordConfig);
+
+            Client.Ready += Client_Ready;
+
+            await Client.ConnectAsync();
+            await Task.Delay(-1); // keeps bot online infinitly ( as long as program is running)
+        }
+
+        private static Task Client_Ready(DiscordClient sender, ReadyEventArgs args)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
